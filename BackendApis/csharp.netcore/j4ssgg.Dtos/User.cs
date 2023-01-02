615fb0e47853dc15b5b8fd9c7a6a7cd503c6e034
A. Gutierrez <j4ssgg@gmail.com>
2023-01-01 19:29:02 -0600

    New project added for Dtos

---
+++ BackendApis/csharp.netcore/j4ssgg.Dtos/User.cs
@@ -0,0 +1,10 @@
+﻿namespace j4ssgg.Dtos
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        private DateTime Registered { get; set; }
+    }
+}
